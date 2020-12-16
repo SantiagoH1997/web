749969741409ef56a76523dcc6f7d9b9e1beb15b
SantiagoH1997 <santiagohernandez.1997@gmail.com>
2020-12-16 18:44:57 -0300

    Update

---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/santiagoh1997/web
+
+go 1.15
+
+require (
+	github.com/go-playground/universal-translator v0.17.0 // indirect
+	github.com/gorilla/mux v1.8.0
+	github.com/leodido/go-urn v1.2.1 // indirect
+	github.com/pkg/errors v0.9.1
+	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.15.1
+	gopkg.in/go-playground/assert.v1 v1.2.1 // indirect
+	gopkg.in/go-playground/validator.v9 v9.31.0
+)
--- web.go
+++ web.go
@@ -8,6 +8,8 @@ import (
 	"time"
 
 	"github.com/gorilla/mux"
+	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
+	"go.opentelemetry.io/otel/trace"
 )
 
 // ctxKey represents the type of value for the context key.
@@ -18,6 +20,7 @@ const KeyValues ctxKey = 1
 
 // Values represent state for each request.
 type Values struct {
+	TraceID    string
 	Now        time.Time
 	StatusCode int
 }
@@ -26,55 +29,32 @@ type Values struct {
 // and can return an error.
 type Handler func(ctx context.Context, w http.ResponseWriter, r *http.Request) error
 
+// registered keeps track of handlers registered to the http default server
+// mux. This is a singleton and used by the standard library for metrics
+// and profiling. The application may want to add other handlers like
+// readiness and liveness to that mux. If this is not tracked, the routes
+// could try to be registered more than once, causing a panic.
+var registered = make(map[string]bool)
+
 // App is the entrypoint into our application and what configures our context
 // object for each of our http handlers.
 type App struct {
-	*mux.Router
+	mux      *mux.Router
+	otmux    http.Handler
 	shutdown chan os.Signal
 	mw       []Middleware
 }
 
 // NewApp creates an App value that handle a set of routes for the application.
 func NewApp(shutdown chan os.Signal, mw ...Middleware) *App {
-	app := App{
-		Router:   mux.NewRouter(),
+	mux := mux.NewRouter()
+
+	return &App{
+		mux:      mux,
+		otmux:    otelhttp.NewHandler(mux, "request"),
 		shutdown: shutdown,
 		mw:       mw,
 	}
-
-	return &app
-}
-
-// Handle is our mechanism for mounting Handlers for a given HTTP verb and path
-func (a *App) Handle(method string, path string, handler Handler, mw ...Middleware) {
-	// First wrap handler specific middleware around this handler
-	// then, the application's general middleware
-	handler = wrapMiddleware(mw, handler)
-	handler = wrapMiddleware(a.mw, handler)
-
-	// The function to execute for each request.
-	h := func(w http.ResponseWriter, r *http.Request) {
-
-		// Set the context with the required values to
-		// process the request.
-		v := Values{
-			Now: time.Now(),
-		}
-		ctx := context.WithValue(context.Background(), KeyValues, &v)
-
-		// Call the wrapped handler functions.
-		if err := handler(ctx, w, r); err != nil {
-			a.SignalShutdown()
-			return
-		}
-	}
-
-	a.Router.HandleFunc(path, h).Methods(method)
-}
-
-// ServeHTTP implements the http.Handler interface.
-func (a *App) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	a.Router.ServeHTTP(w, r)
 }
 
 // SignalShutdown is used to gracefully shutdown the app when an
@@ -83,7 +63,81 @@ func (a *App) SignalShutdown() {
 	a.shutdown <- syscall.SIGTERM
 }
 
+// ServeHTTP implements the http.Handler interface.
+func (a *App) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	a.otmux.ServeHTTP(w, r)
+}
+
 // Params returns the web call parameters from the request.
 func Params(r *http.Request) map[string]string {
 	return mux.Vars(r)
+}
+
+// HandleDebug sets a handler function for a given HTTP method and path pair
+// to the default http package server mux. /debug is added to the path.
+func (a *App) HandleDebug(method string, path string, handler Handler, mw ...Middleware) {
+	a.handle(true, method, path, handler, mw...)
+}
+
+// Handle sets a handler function for a given HTTP method and path pair
+// to the application server mux.
+func (a *App) Handle(method string, path string, handler Handler, mw ...Middleware) {
+	a.handle(false, method, path, handler, mw...)
+}
+
+// handle performs the real work of applying boilerplate and framework code
+// for a handler.
+func (a *App) handle(debug bool, method string, path string, handler Handler, mw ...Middleware) {
+	if debug {
+		// Track all the handlers that are being registered so we don't have
+		// the same handlers registered twice to this singleton.
+		if _, exists := registered[method+path]; exists {
+			return
+		}
+		registered[method+path] = true
+	}
+	// First wrap handler specific middleware around this handler.
+	handler = wrapMiddleware(mw, handler)
+
+	// Add the application's general middleware to the handler chain.
+	handler = wrapMiddleware(a.mw, handler)
+
+	// The function to execute for each request.
+	h := func(w http.ResponseWriter, r *http.Request) {
+
+		// Start or expand a distributed trace.
+		ctx := r.Context()
+		ctx, span := trace.SpanFromContext(ctx).Tracer().Start(ctx, r.URL.Path)
+		defer span.End()
+
+		// Set the context with the required values to
+		// process the request.
+		v := Values{
+			TraceID: span.SpanContext().TraceID.String(),
+			Now:     time.Now(),
+		}
+		ctx = context.WithValue(ctx, KeyValues, &v)
+
+		// Call the wrapped handler functions.
+		if err := handler(ctx, w, r); err != nil {
+			a.SignalShutdown()
+			return
+		}
+	}
+
+	// Add this handler for the specified verb and route.
+	if debug {
+		f := func(w http.ResponseWriter, r *http.Request) {
+			switch {
+			case r.Method == method:
+				h(w, r)
+			default:
+				w.WriteHeader(http.StatusNotFound)
+			}
+		}
+		http.DefaultServeMux.HandleFunc("/debug"+path, f)
+		return
+	}
+
+	a.mux.HandleFunc(path, h).Methods(method)
 }
